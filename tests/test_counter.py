4671db996947297e197460b7cb8ba7d910b914e3
Kyle Rainey <rainek1@unlv.nevada.edu>
2024-10-06 18:16:02 -0700

    Added some helper functions to reduce code duplication.
    Based on this ChatGPT convo:
    https://chatgpt.com/share/67033541-8510-8000-921e-ad6f505bba8e

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -1,10 +1,6 @@
 import pytest
 import uuid
-
-# we need to import the unit under test - counter
 from src.counter import app 
-
-# we need to import the file that contains the status codes
 from src import status 
 
 @pytest.fixture()
@@ -20,45 +16,50 @@ def unique_counter_name():
 class TestCounterEndPoints:
     """Test cases for Counter-related endpoints"""
 
+    def create_counter(self, client, counter_name):
+        return client.post(f'/counters/{counter_name}')
+
+    def get_counter(self, client, counter_name):
+        return client.get(f'/counters/{counter_name}')
+
+    def update_counter(self, client, counter_name):
+        return client.put(f'/counters/{counter_name}')
+
+    def delete_counter(self, client, counter_name):
+        return client.delete(f'/counters/{counter_name}')
+
     def test_create_a_counter(self, client, unique_counter_name):
         """It should create a counter"""
-        result = client.post(f'/counters/{unique_counter_name}')
+        result = self.create_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_201_CREATED     
 
     def test_duplicate_a_counter(self, client, unique_counter_name):
         """It should return an error for duplicates"""
-        result = client.post(f'/counters/{unique_counter_name}')
-        assert result.status_code == status.HTTP_201_CREATED
-        result = client.post(f'/counters/{unique_counter_name}')
+        self.create_counter(client, unique_counter_name)
+        result = self.create_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_409_CONFLICT
     
     def test_update_a_counter(self, client, unique_counter_name):
-        result = client.post(f'/counters/{unique_counter_name}')
-        assert result.status_code == status.HTTP_201_CREATED
-        original_count = result.json[unique_counter_name]
-        result = client.put(f'/counters/{unique_counter_name}')
+        self.create_counter(client, unique_counter_name)
+        result = self.update_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_200_OK
         new_count = result.json[unique_counter_name]
-        assert original_count + 1 == new_count
+        assert new_count == 1  # Assuming the counter increments by 1
 
     def test_get_counter(self, client, unique_counter_name):
-        result = client.post(f'/counters/{unique_counter_name}')
-        assert result.status_code == status.HTTP_201_CREATED
-        original_count = result.json[unique_counter_name]
-        result = client.get(f'/counters/{unique_counter_name}')
+        self.create_counter(client, unique_counter_name)
+        result = self.get_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_200_OK
-        returned_count = result.json[unique_counter_name]
-        assert original_count == returned_count
+        assert result.json[unique_counter_name] == 0  # Assuming initial value is 0
 
     def test_del_counter(self, client, unique_counter_name):
-        result = client.post(f'/counters/{unique_counter_name}')
-        assert result.status_code == status.HTTP_201_CREATED
-        result = client.delete(f'/counters/{unique_counter_name}')
+        self.create_counter(client, unique_counter_name)
+        result = self.delete_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_204_NO_CONTENT
-        result = client.get(f'/counters/{unique_counter_name}')
+        result = self.get_counter(client, unique_counter_name)
         assert result.status_code == status.HTTP_404_NOT_FOUND
 
     def test_del_counter_not_present(self, client):
-        counter_name = f'/counters/counter-{uuid.uuid4()}'
-        result = client.delete(counter_name)
+        counter_name = f'counter-{uuid.uuid4()}'
+        result = self.delete_counter(client, counter_name)
         assert result.status_code == status.HTTP_404_NOT_FOUND
